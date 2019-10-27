019378595454a513a1de263868ea42b0c291fd4c
marin9 <marin.bralic09@gmail.com>
2019-10-28 00:08:50 +0100

    Update

--- main.c
+++ main.c
@@ -3,28 +3,18 @@
 #include <unistd.h>
 #include "graphic.h"
 #include "input.h"
+#include "main.h"
 
 
-
-int menu_step(){
-
-	return 0;
-}
-
-void menu_rend(){
-
-}
-
-
-
-int current=0;
+static int current;
 
 struct game{
 	char name[16];
-	int (*step)();
+	void (*step)();
 	void (*rend)();
 } apps[] = {
-	{"Menu", menu_step, menu_rend}
+	{"Menu", menu_step, menu_rend},
+	{"", 0, 0}
 };
 
 
@@ -48,18 +38,37 @@ int main(int argc, char **argv){
 		}
 	}
 
+	current=0;
 	graphic_init(pflag);
-
-	while(1){
-		if(apps[current].step()){
-			current=0;
-			continue;
-		}
-		apps[current].rend();
+	while(current>=0){
+		apps[current].step();
 		graphic_refresh();
 		usleep(20000);
 	}
-
 	graphic_close();
 	return 0;
+}
+
+void menu_set(int n){
+	current=n;
+}
+
+void menu_step(){
+	int k=input_getkey();
+	
+	if(k==EXIT){
+		current=-1;
+	}
+
+	menu_rend();
+}
+
+void menu_rend(){
+	graphic_clear(0);
+	graphic_setcolor(GREEN);
+	graphic_drawrect(0, 0, 42, 128);
+	graphic_setcolor(WHITE);
+	graphic_drawrect(42, 0, 44, 128);
+	graphic_setcolor(BROWN);
+	graphic_drawrect(86, 0, 42, 128);
 }
---
+++ main.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void menu_set(int n);
+void menu_step();
+void menu_rend();
