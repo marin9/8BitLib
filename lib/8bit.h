7f8d403621ba4a56ed1b01fe2bad92e7e2038b4f
marin9 <marin.bralic09@gmail.com>
2019-11-04 23:04:33 +0100

    Update

---
+++ lib/8bit.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "system.h"
+#include "graphic.h"
+#include "input.h"
+#include "audio.h"
+
--- test/test.c
+++ test/test.c
@@ -1,26 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <SDL2/SDL.h>
-#include "system.h"
-#include "graphic.h"
-#include "input.h"
-#include "audio.h"
-
+#include "8bit.h"
 
 void menu_rend(){
+	int i;
 	graphic_clear(0);
-	graphic_setcolor(DARK_GREEN);
-	graphic_drawrect(0, 0, 42, 128);
-	graphic_setcolor(WHITE);
-	graphic_drawrect(42, 0, 44, 128);
-	graphic_setcolor(ORANGE);
-	graphic_drawrect(86, 0, 42, 128);
+
+	for(i=0;i<16;++i){
+		graphic_setcolor(i);
+		graphic_drawrect(i*8, 0, 128, 128);
+	}
 
 	graphic_setcolor(BLACK);
 	graphic_setfontsize(1);
 	graphic_drawtext(10, 10, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 	graphic_drawtext(10, 30, "abcdefghijklmnopqrstuvwxyz");
+	graphic_drawtext(10, 50, "       0123456789");
 }
 
 
